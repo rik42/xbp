2b19c00b2bdfcb75f6aa6ec9213537bef1220c2b
rik42 <r202202024eq@jindai.jp>
2022-12-30 01:05:27 +0900

    1229dezifab

---
+++ digi_fab/images/de12/python/game.py
@@ -0,0 +1,32 @@
+name=input("名前を入力してください")
+month=int(input("誕生月を入力してください"))
+
+if   month==1:
+      print(name,"さんは性格が温厚です")
+if month==2:
+     print(name,"さんは積極的です")
+if month==3:
+      print(name,"さんは社交的です")
+if month==4:
+      print(name,"さんは自分に厳しく、相手に優しい性格です")
+if month==5:
+     print(name,"さんは情熱的で抜群の行動力があります")
+if month==6:
+     print(name,"さんは強い好奇心と行動力があります")
+if month==7:
+     print(name,"さんは思いやりがあり、優しいです")
+if month==8:
+     print(name,"さんは華やかな性格で、新しいものが好きです")
+if month==9:
+     print(name,"さんは完璧主義で繊細な心を持っています")
+if month==10:
+     print(name,"さんは責任感があり、まじめです")
+if month==11:
+     print(name,"さんはマニアックです")
+if month==12:
+     print(name,"さんは素直でマイペースです")
+if month<=0:
+     print(name,"さんはｒerror")
+if month>=13:
+     print(name,"さんはerror")
+
